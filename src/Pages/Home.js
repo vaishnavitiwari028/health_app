07693a6c73af7195792a74a5a5f3cbdcefb95ec4
vaishnavitiwari028 <vaishnavitiwari028@gmail.com>
2024-05-20 23:59:11 +0530

    Info and Hero Section Added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import Hero from "../Components/Hero";
+import Info from "../Components/Info";
 import Footer from "../Components/Footer";
 
 function Home() {
   return (
     <div className="home-section">
       <Navbar />
+      <Hero />
+      <Info />
       <Footer />
     </div>
   );
