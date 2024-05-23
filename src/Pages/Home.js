2d5e01d1c5c964351245bcda6731cc849d628028
vaishnavitiwari028 <vaishnavitiwari028@gmail.com>
2024-05-23 23:56:34 +0530

    Review Page Added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,8 @@ import Hero from "../Components/Hero";
 import Info from "../Components/Info";
 import About from "../Components/About";
 import BookAppointment from "../Components/BookAppointment";
+import Reviews from "../Components/Reviews";
+
 import Footer from "../Components/Footer";
 
 function Home() {
@@ -14,6 +16,8 @@ function Home() {
       <Info />
       <About />
       <BookAppointment />
+      <Reviews />
+
       <Footer />
     </div>
   );
